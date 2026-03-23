f9f8fa5bc769fa83056e7daa79bae24d37b858e6
quynh150105 <quynh1512005@gmail.com>
2026-03-23 20:48:07 +0700

    add fist data

---
+++ LapTrinhGUI/src/main/java/com/mycompany/laptrinhgui/LapTrinhGUI.java
@@ -0,0 +1,12 @@
+package com.mycompany.laptrinhgui;
+
+/**
+ *
+ * @author Smileboiz
+ */
+public class LapTrinhGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
